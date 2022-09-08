df6cc0f2f69c15a502e6c23fab088ae3fb3633be
Yur1yPzdc <96118519+Yur1yPzdc@users.noreply.github.com>
2022-09-08 12:25:07 +0700

    Update test_I.py

--- test_I.py
+++ test_I.py
@@ -1,11 +1,12 @@
 skobki = {}
 line = ''.join([i for i in input() if i not in '1234567890-+/*'])
 case = ''
-for i in range(int(input())):
+for _ in range(int(input())):
+    i = input()
     skobki[i[-1]] = i[0]
 while True:
-    q = i
     for i in range(len(line)):
+        q = i
         if i in skobki.keys():
             qq = skobki[line[i]]
             q -= 1
@@ -17,9 +18,27 @@ while True:
                 line = line[:q] + line[q+1:]
 
             if line[q] != qq:
-                case = f'Miss open bracket for bracket {qq}'
+                case = f'Expected {qq} but {line[q]} found'
+
             if bool(len(case)):
                 break
+
+    w = True
+    for i in list(skobki.keys()):
+        if i in line:
+            w = False
+            break
+        else:
+            if i == list(skobki.keys()[-1]):
+                case = f'Miss сlose bracket for bracket {line[-1]}'
+            
     if bool(len(case)):
-                break
-    
+        break
+
+    if len(line):
+        break
+
+if bool(len(case)):
+    print(case)
+else:
+    print('Correct')
